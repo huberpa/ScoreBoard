653df1018d93be4269b24fb5a415ed11e8eb8001
Patrick Huber <patrick.huber.gaggenau@freenet.de>
2013-04-11 10:41:40 +0200

    zrsgfsddsf

---
+++ Tippspiel/src/com/example/tippspiel/Tippen.java
@@ -0,0 +1,15 @@
+package com.example.tippspiel;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tippen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tippen);
+	
+		
+	}
+}
