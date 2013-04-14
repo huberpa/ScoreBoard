12ad55b412a7a2df955b59aa87316fa38748dcb7
Patrick Huber <patrick.huber.gaggenau@freenet.de>
2013-04-14 11:28:09 +0200

    v 0.2

--- ScoreBoard/gen/com/example/scoreboard/R.java
+++ ScoreBoard/gen/com/example/scoreboard/R.java
@@ -12,10 +12,12 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int logo_scoreboard=0x7f020001;
     }
     public static final class id {
         public static final int button=0x7f070003;
-        public static final int menu_settings=0x7f070005;
+        public static final int imageView1=0x7f070005;
+        public static final int menu_settings=0x7f070006;
         public static final int name=0x7f070000;
         public static final int pwfeld=0x7f070004;
         public static final int textView1=0x7f070001;
@@ -24,6 +26,7 @@ public final class R {
     public static final class layout {
         public static final int activity_login=0x7f030000;
         public static final int activity_main=0x7f030001;
+        public static final int activity_start=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
